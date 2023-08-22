013c691e242e0147c163920e68f0147948938eda
vmalepati1 <malepativikas@gmail.com>
2023-08-21 22:47:43 -0400

    Fixed issue with bootstrap

---
+++ build/precache-manifest.9810b5485b07c19ab7ce5ceeb22df68f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1c7b5bea5532943bc8cea0bc5fd707",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "417da2e1b1ef36573d69",
+    "url": "/main/static/css/2.fea3aa44.chunk.css"
+  },
+  {
+    "revision": "4298e3d7177fe5460502",
+    "url": "/main/static/css/main.696877ea.chunk.css"
+  },
+  {
+    "revision": "417da2e1b1ef36573d69",
+    "url": "/main/static/js/2.d84a6eb0.chunk.js"
+  },
+  {
+    "revision": "67e209d5ff7b6c671d25e55b2bf0dc41",
+    "url": "/main/static/js/2.d84a6eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4298e3d7177fe5460502",
+    "url": "/main/static/js/main.9c5f968d.chunk.js"
+  },
+  {
+    "revision": "12af6743f01091f43902",
+    "url": "/main/static/js/runtime-main.79e94ead.js"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/main/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "32442a8e647bf33b852807cd91c492e4",
+    "url": "/main/static/media/laravel-logo.32442a8e.svg"
+  },
+  {
+    "revision": "edf25cc2d67d8e667b4455abcb435cfc",
+    "url": "/main/static/media/logo-192x192.edf25cc2.png"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/main/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "f7d45c01a0efd126cc1030d5cf78fd84",
+    "url": "/main/static/media/react-logo-transparent.f7d45c01.svg"
+  },
+  {
+    "revision": "b779e4fd2515e28f0233a071f3d46104",
+    "url": "/main/static/media/react-logo.b779e4fd.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/main/static/media/themesberg.105e5059.svg"
+  }
+]);
